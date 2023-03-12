84203327df3dfa59287a22990cd5e5b0c92c4901
Mohammad Shaad Shaikh <mohammadshaadshaikh@gmail.com>
2023-03-12 15:23:05 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <h2 class="text-red-800">
+      Hello World!
+    </h2>
+  </header>
+
+  <main>
+    <h4 class="text-sm">
+      How are you?
+    </h4>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{vue,tsx,jsx,ts,html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
